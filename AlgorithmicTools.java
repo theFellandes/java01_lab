bff7daa7161942a69af3995d4bcff8edc2fdc009
theFellandes <57584110+theFellandes@users.noreply.github.com>
2020-08-26 21:07:38 +0300

    Add files via upload

---
+++ AlgorithmicTools.java
@@ -0,0 +1,18 @@
+import lab.TestEnvironment;
+import java.util.Arrays;
+
+public class AlgorithmicTools {
+
+    public static void main(String[] args){
+//        System.out.println("test: " + week2.FibonacciUtils.listFibonacci(3));
+//        System.out.println(Arrays.toString(week2.FibonacciUtils.fibonacciArray(3)));
+//        System.out.println(Arrays.toString(lab.TestEnvironment.digitSeperator(1234)));
+//        System.out.println(lab.TestEnvironment.digitSum(TestEnvironment.digitSeperator(1234)));
+//        System.out.println(week2.FibonacciUtils.euclideanGCD(357, 234));
+//        System.out.println(week2.FibonacciUtils.lastFibonacci(8));
+//          System.out.println(week2.FibonacciUtils.improvedFibonacciSum(100000000));  //overflow
+//        System.out.println(week2.FibonacciUtils.improvedLastDigitSum(100000000));
+        System.out.println(week2.FibonacciUtils.improvedSquaredDigitSum(2));
+    }
+
+}
